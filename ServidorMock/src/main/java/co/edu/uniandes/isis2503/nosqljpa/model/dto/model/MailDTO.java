d06c0c2f02ec89848c8d641e0b8843f6902ed565
LAPTOP-UICF0P4O\Mariana <m.rodriguez21@uniandes.edu.co>
2018-03-25 21:40:37 -0500

    Servidor mock y programa P actualizado

---
+++ ServidorMock/src/main/java/co/edu/uniandes/isis2503/nosqljpa/model/dto/model/MailDTO.java
@@ -0,0 +1,84 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Universidad De Los Andes - Departamento de Ingeniería de Sistemas.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package co.edu.uniandes.isis2503.nosqljpa.model.dto.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ca.mendoza968
+ */
+@XmlRootElement
+public class MailDTO {
+    private String asunto;
+    private String remitente;
+    private String cuerpo;
+    private List<String> destinatarios;
+
+    public MailDTO() {
+        this.destinatarios = new ArrayList();
+    }
+
+    public MailDTO(String asunto, String remitente, String cuerpo, List<String> destinatarios) {
+        this.asunto = asunto;
+        this.remitente = remitente;
+        this.cuerpo = cuerpo;
+        this.destinatarios= destinatarios;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+
+    public void setAsunto(String name) {
+        this.asunto = name;
+    }
+    
+    public String getRemitente() {
+        return remitente;
+    }
+
+    public void setRemitente(String id) {
+        this.remitente = id;
+    }
+
+    public List<String> getDestinatarios() {
+        return destinatarios;
+    }
+
+    public void setDestinatarios(List<String> destinatarios) {
+        this.destinatarios = destinatarios;
+    }
+    
+}
